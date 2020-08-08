3e46ee4b097d937e10691ae15ab14409f89571f7
fboisier <francisco.boisier.sandoval@gmail.com>
2020-08-08 11:28:14 -0400

    Primer Commit.

---
+++ dojoexam/src/main/java/com/fboisier/dojoexam/DojoexamApplication.java
@@ -0,0 +1,13 @@
+package com.fboisier.dojoexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoexamApplication.class, args);
+	}
+
+}
---
+++ dojoexam/src/test/java/com/fboisier/dojoexam/DojoexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fboisier.dojoexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
